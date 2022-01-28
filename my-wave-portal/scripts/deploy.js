04cf1aa035af82909666636bd80b79ad3ac399e6
Gabriel <gabriel@gabrielhorvat.com>
2022-01-28 16:01:04 +0200

    deploy script

--- my-wave-portal/scripts/deploy.js
+++ my-wave-portal/scripts/deploy.js
@@ -23,3 +23,10 @@ const runMain = async () => {
 };
 
 runMain();
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
