9285e1df4db66956adbb471a0bd830dbf3fac1ec
Gabriel <gabriel@gabrielhorvat.com>
2022-02-01 12:35:24 +0200

    amend contract to send/store msgs

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,19 +5,39 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    uint256 totalWaves;
+  uint256 totalWaves;
 
-    constructor() {
-        console.log("Im sexy and I know it :)");
-    }
+  // make new event
+  event NewWave(address indexed from, uint256 timestamp, string message);
 
-    function wave() public {
-        totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
-    }
+  struct Wave {
+    address waver; // The address of the user who waved.
+    string message; // The message the user sent.
+    uint256 timestamp; // The timestamp when the user waved.
+  }
 
-    function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
-        return totalWaves;
-    }
+  Wave[] waves;
+
+  constructor() {
+    console.log("I AM SMART CONTRACT. POG.");
+  }
+
+  function wave(string memory _message) public {
+    totalWaves += 1;
+    console.log("%s waved w/ message %s", msg.sender, _message);
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    // "return" NewWave value
+    emit NewWave(msg.sender, block.timestamp, _message);
+  }
+
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
+  }
 }
