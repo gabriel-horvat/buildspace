179de1b7b3caef8c959f8ce0cc73feb712d9e870
Gabriel <gabriel@gabrielhorvat.com>
2022-02-01 15:26:04 +0200

    update contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,19 +7,18 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
-  // make new event
   event NewWave(address indexed from, uint256 timestamp, string message);
 
   struct Wave {
-    address waver; // The address of the user who waved.
-    string message; // The message the user sent.
-    uint256 timestamp; // The timestamp when the user waved.
+    address waver;
+    string message;
+    uint256 timestamp;
   }
 
   Wave[] waves;
 
-  constructor() {
-    console.log("I AM SMART CONTRACT. POG.");
+  constructor() payable {
+    console.log("We have been constructed!");
   }
 
   function wave(string memory _message) public {
@@ -30,6 +29,14 @@ contract WavePortal {
 
     // "return" NewWave value
     emit NewWave(msg.sender, block.timestamp, _message);
+
+    uint256 prizeAmount = 0.0001 ether;
+    require(
+      prizeAmount <= address(this).balance,
+      "Trying to withdraw more money than the contract has."
+    );
+    (bool success, ) = (msg.sender).call{ value: prizeAmount }("");
+    require(success, "Failed to withdraw money from contract.");
   }
 
   function getAllWaves() public view returns (Wave[] memory) {
@@ -37,7 +44,6 @@ contract WavePortal {
   }
 
   function getTotalWaves() public view returns (uint256) {
-    console.log("We have %d total waves!", totalWaves);
     return totalWaves;
   }
 }
