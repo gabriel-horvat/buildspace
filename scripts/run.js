d0beb720a03d50555c47048f50bcc41a4f37bd5c
Gabriel <gabriel@gabrielhorvat.com>
2022-02-03 10:33:21 +0200

    update run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy({
-    value: hre.ethers.utils.parseEther("0.01"),
+    value: hre.ethers.utils.parseEther("0.1"),
   });
   await waveContract.deployed();
   console.log("Contract addy:", waveContract.address);
@@ -14,10 +14,16 @@ const main = async () => {
     hre.ethers.utils.formatEther(contractBalance)
   );
 
-  let waveTxn = await waveContract.wave("A message!");
+  /*
+   * Let's try two waves now
+   */
+  const waveTxn = await waveContract.wave("This is wave #1");
   await waveTxn.wait();
 
-  contractBalance = await hre.ethers.provider.getBalance(waveContract.addresss);
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log(
     "Contract balance:",
     hre.ethers.utils.formatEther(contractBalance)
