cad124d4e887c4d8b40ff611e4c224a0df73771f
Gabriel <gabriel@gabrielhorvat.com>
2022-02-03 16:48:18 +0200

    contract fix

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -22,8 +22,6 @@ contract WavePortal {
 
   constructor() payable {
     console.log("We have been constructed!");
-
-    seed = (block.timestamp + block.difficulty) % 100;
   }
 
   function wave(string memory _message) public {
@@ -36,15 +34,18 @@ contract WavePortal {
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
-    seed = (block.difficulty + block.timestamp + seed) % 100;
+    uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("Random # generated: %s", randomNumber);
 
-    if (seed <= 50) {
+    seed = randomNumber;
+
+    if (randomNumber < 50) {
       console.log("%s won!", msg.sender);
 
       uint256 prizeAmount = 0.0001 ether;
       require(
         prizeAmount <= address(this).balance,
-        "Trying to withdraw more money than they contract has."
+        "Trying to withdraw more money than the contract has."
       );
       (bool success, ) = (msg.sender).call{ value: prizeAmount }("");
       require(success, "Failed to withdraw money from contract.");
