b146290e9d8f8bf3bc09d1278efbcee639c3e1c7
Gabriel <gabriel@gabrielhorvat.com>
2022-02-03 10:46:46 +0200

    add cool down anti spam func

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,6 @@ import "hardhat/console.sol";
 
 contract WavePortal {
   uint256 totalWaves;
-
   uint256 private seed;
 
   event NewWave(address indexed from, uint256 timestamp, string message);
@@ -19,6 +18,8 @@ contract WavePortal {
 
   Wave[] waves;
 
+  mapping(address => uint256) public lastWavedAt;
+
   constructor() payable {
     console.log("We have been constructed!");
 
@@ -26,18 +27,16 @@ contract WavePortal {
   }
 
   function wave(string memory _message) public {
+    require(lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15m");
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
     totalWaves += 1;
-    console.log("%s waved w/ message %s", msg.sender, _message);
+    console.log("%s has waved!", msg.sender);
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
     seed = (block.difficulty + block.timestamp + seed) % 100;
-
-    console.log("Random # generated: %d", seed);
-
-    /*
-     * Give a 50% chance that the user wins the prize.
-     */
 
     if (seed <= 50) {
       console.log("%s won!", msg.sender);
@@ -45,13 +44,12 @@ contract WavePortal {
       uint256 prizeAmount = 0.0001 ether;
       require(
         prizeAmount <= address(this).balance,
-        "Trying to withdraw more money than the contract has."
+        "Trying to withdraw more money than they contract has."
       );
       (bool success, ) = (msg.sender).call{ value: prizeAmount }("");
       require(success, "Failed to withdraw money from contract.");
     }
 
-    // "return" NewWave value
     emit NewWave(msg.sender, block.timestamp, _message);
   }
 
